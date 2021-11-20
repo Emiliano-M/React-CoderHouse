676bd6eb45c2f8306d4509f74d5e57905feab7f8
Emiliano <emiliano0montes@gmail.com>
2021-11-20 00:11:24 -0300

    Bug Fix

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -2,9 +2,9 @@
 const Cart = () => {
 
     return (
-        <button type="button" class="btn btn-light"> <img src="./img/cart.svg" class="img-fluid" alt= "Carro de compras"/> </button>
+        <button type="button" className="btn btn-light"> <img src="./img/cart.svg" className="img-fluid" alt= "Carro de compras"/> </button>
     )
 
 }
 
-export default Cart 
+export default Cart;
